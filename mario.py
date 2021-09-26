2ccd3e60bc027bfa54cdad5461972d8afc37bd4d
MahatKC <mkarvat@gmail.com>
2021-09-26 20:32:31 -0300

    changes

--- mario.py
+++ mario.py
@@ -1,7 +1,7 @@
 #Amanda Israel Graeff Borges, Lucas Veit de Sá e Mateus Karvat Camara
 import numpy as np
 from numpy.random import default_rng
-import time
+import time, copy
 import pandas as pd
 import random
 import os
@@ -51,62 +51,82 @@ class environment:
         
         return state_full
 
-    def step(self,action):
+    def step(self,action, reduced_action):
+        if not reduced_action:
+            if action == 0: # Pular       
+                self.pyboy.send_input(WindowEvent.RELEASE_ARROW_DOWN)
+                self.pyboy.send_input(WindowEvent.PRESS_BUTTON_A)
+                self.time = 2
 
-        if action == 0: # Pular       
-            self.pyboy.send_input(WindowEvent.RELEASE_ARROW_DOWN)
-            self.pyboy.send_input(WindowEvent.PRESS_BUTTON_A)
-            self.time = 2
+            elif action == 1: # Soltar Pular
+                self.pyboy.send_input(WindowEvent.RELEASE_ARROW_DOWN)
+                self.pyboy.send_input(WindowEvent.RELEASE_BUTTON_A)
+                self.time = 2
 
-        elif action == 1: # Soltar Pular
-            self.pyboy.send_input(WindowEvent.RELEASE_ARROW_DOWN)
-            self.pyboy.send_input(WindowEvent.RELEASE_BUTTON_A)
-            self.time = 2
+            elif action == 2: # Andar para esquerda
+                self.pyboy.send_input(WindowEvent.RELEASE_ARROW_RIGHT)
+                self.pyboy.send_input(WindowEvent.PRESS_ARROW_LEFT)
+                self.time = 2
 
-        elif action == 2: # Andar para esquerda
-            self.pyboy.send_input(WindowEvent.RELEASE_ARROW_RIGHT)
-            self.pyboy.send_input(WindowEvent.PRESS_ARROW_LEFT)
-            self.time = 2
+            elif action == 3: # Parar de andar para esquerda
+                self.pyboy.send_input(WindowEvent.RELEASE_ARROW_LEFT)
+                self.time = 2
 
-        elif action == 3: # Parar de andar para esquerda
-            self.pyboy.send_input(WindowEvent.RELEASE_ARROW_LEFT)
-            self.time = 2
+            elif action == 4: # Andar para direita
+                self.pyboy.send_input(WindowEvent.RELEASE_ARROW_LEFT)
+                self.pyboy.send_input(WindowEvent.PRESS_ARROW_RIGHT)
+                self.time = 2
 
-        elif action == 4: # Andar para direita
-            self.pyboy.send_input(WindowEvent.RELEASE_ARROW_LEFT)
-            self.pyboy.send_input(WindowEvent.PRESS_ARROW_RIGHT)
-            self.time = 2
+            elif action == 5: # Parar de andar para direita
+                self.pyboy.send_input(WindowEvent.RELEASE_ARROW_RIGHT)
+                self.time = 2
 
-        elif action == 5: # Parar de andar para direita
-            self.pyboy.send_input(WindowEvent.RELEASE_ARROW_RIGHT)
-            self.time = 2
+            elif action == 6: # Apertar Correr / Fogo
+                self.pyboy.send_input(WindowEvent.PRESS_BUTTON_B)
+                self.time = 2
 
-        elif action == 6: # Apertar Correr / Fogo
-            self.pyboy.send_input(WindowEvent.PRESS_BUTTON_B)
-            self.time = 2
+            elif action == 7: # Soltar Correr / Fogo
+                self.pyboy.send_input(WindowEvent.RELEASE_BUTTON_B)
+                self.time = 2
 
-        elif action == 7: # Soltar Correr / Fogo
-            self.pyboy.send_input(WindowEvent.RELEASE_BUTTON_B)
-            self.time = 2
+            elif action == 8: # Apertar para Baixo
+                self.pyboy.send_input(WindowEvent.RELEASE_BUTTON_A)
+                self.pyboy.send_input(WindowEvent.PRESS_ARROW_DOWN)
+                self.time = 2
 
-        elif action == 8: # Apertar para Baixo
-            self.pyboy.send_input(WindowEvent.RELEASE_BUTTON_A)
-            self.pyboy.send_input(WindowEvent.PRESS_ARROW_DOWN)
-            self.time = 2
+            elif action == 9: # Soltar para Baixo
+                self.pyboy.send_input(WindowEvent.RELEASE_BUTTON_A)
+                self.pyboy.send_input(WindowEvent.RELEASE_ARROW_DOWN)
+                self.time = 2
+        else:
+            if action == 0: # Pular       
+                self.pyboy.send_input(WindowEvent.PRESS_BUTTON_A)
+                self.time = 2
 
-        elif action == 9: # Soltar para Baixo
-            self.pyboy.send_input(WindowEvent.RELEASE_BUTTON_A)
-            self.pyboy.send_input(WindowEvent.RELEASE_ARROW_DOWN)
-            self.time = 2
+            elif action == 1: # Soltar Pular
+                self.pyboy.send_input(WindowEvent.RELEASE_BUTTON_A)
+                self.time = 2
+
+            elif action == 2: # Andar para direita
+                self.pyboy.send_input(WindowEvent.PRESS_ARROW_RIGHT)
+                self.time = 2
+
+            elif action == 3: # Parar de andar para direita
+                self.pyboy.send_input(WindowEvent.RELEASE_ARROW_RIGHT)
+                self.time = 2
                  
         return action, self.time
 
 class Network():
-    def __init__(self, cromossome_size):
+    def __init__(self, cromossome_size, reduced_action):
         # Gera cromossomo com sequência aleatória de ações
-        self.actions = np.random.randint(0, 10, size=cromossome_size).tolist()
+        if reduced_action:
+            self.actions = np.random.randint(0, 4, size=cromossome_size).tolist()
+        else:
+            self.actions = np.random.randint(0, 10, size=cromossome_size).tolist()
         #self.actions = [4]*100+[0]*3+[1]+[5]+[2]*15+[3]+[0]+[5]*400
         self.generation = 0
+        self.death_iteration = 0
 
         self.lucro = 0
 
@@ -118,10 +138,10 @@ class Network():
         self.lucro = lucro
         return self.lucro
 
-def init_networks(population, cromossome_size):
-    return [Network(cromossome_size) for _ in range(population)]
+def init_networks(population, cromossome_size, reduced_action):
+    return [Network(cromossome_size, reduced_action) for _ in range(population)]
 
-def fitness(networks):
+def fitness(networks, reduced_action):
     for network in networks:
         #init env
         env = environment()
@@ -149,7 +169,7 @@ def fitness(networks):
                 #print(iterator)
                 #break
 
-            act, tempo = env.step(act)
+            act, tempo = env.step(act, reduced_action)
             
             state = np.asarray(env.mario.game_area()) 
             position = env.mario.level_progress
@@ -173,6 +193,7 @@ def fitness(networks):
             fitness = env.mario.fitness
             
             if env.mario.lives_left < vidas:
+                network.death_iteration = action_counter
                 done = True
                 break
             
@@ -192,55 +213,115 @@ def selection(networks, population, selection_percentage):
 
     return networks
 
-def crossover(networks, self_crossover, population, selection_percentage, cromossome_size):
-    children = []   
-    num_pares_filhos = int(population*((1-selection_percentage)/2))
-    for _ in range(num_pares_filhos):
-        parent1 = random.choice(networks)
-        parent2 = random.choice(networks)
+def crossover(networks, self_crossover, population, selection_percentage, cromossome_size, death_mutation, reduced_action):
+    children = []
+    if death_mutation:
+        num_pares_filhos = int(population*((1-selection_percentage)/4))
+        for _ in range(num_pares_filhos):
+            parent1 = random.choice(networks)
+            parent2 = random.choice(networks)
 
-        if not self_crossover:
-            while parent1==parent2:
-                parent1 = random.choice(networks)
-                parent2 = random.choice(networks)
+            if not self_crossover:
+                while parent1==parent2:
+                    parent1 = random.choice(networks)
+                    parent2 = random.choice(networks)
 
-        child1 = Network(cromossome_size)
-        child2 = Network(cromossome_size)
+            child1 = Network(cromossome_size, reduced_action)
+            child2 = Network(cromossome_size, reduced_action)
+            child3 = copy.deepcopy(parent1)
+            child4 = copy.deepcopy(parent2)
 
-        # Escolha aleatória do ponto de crossover
-        n = np.random.randint(0,cromossome_size)
+            # Escolha aleatória do ponto de crossover
+            n = np.random.randint(0,cromossome_size)
 
-        # Crossover dos pais
-        p1_beginning = parent1.actions[:n]
-        p1_end = parent1.actions[n:]
-        p2_beginning = parent2.actions[:n]
-        p2_end = parent2.actions[n:]
-        
-        child1.actions = p1_beginning+p2_end
-        child2.actions = p2_beginning+p1_end
+            # Crossover dos pais
+            p1_beginning = parent1.actions[:n]
+            p1_end = parent1.actions[n:]
+            p2_beginning = parent2.actions[:n]
+            p2_end = parent2.actions[n:]
+            
+            child1.actions = p1_beginning+p2_end
+            child2.actions = p2_beginning+p1_end
 
-        children.append(child1)
-        children.append(child2)
-        
+            children.append(child1)
+            children.append(child2)
+            children.append(child3)
+            children.append(child4)
+    else:
+        num_pares_filhos = int(population*((1-selection_percentage)/2))
+        for _ in range(num_pares_filhos):
+            parent1 = random.choice(networks)
+            parent2 = random.choice(networks)
+
+            if not self_crossover:
+                while parent1==parent2:
+                    parent1 = random.choice(networks)
+                    parent2 = random.choice(networks)
+
+            child1 = Network(cromossome_size)
+            child2 = Network(cromossome_size)
+
+            # Escolha aleatória do ponto de crossover
+            n = np.random.randint(0,cromossome_size)
+
+            # Crossover dos pais
+            p1_beginning = parent1.actions[:n]
+            p1_end = parent1.actions[n:]
+            p2_beginning = parent2.actions[:n]
+            p2_end = parent2.actions[n:]
+            
+            child1.actions = p1_beginning+p2_end
+            child2.actions = p2_beginning+p1_end
+
+            children.append(child1)
+            children.append(child2)
     networks.extend(children)
     return networks
 
-def mutate(networks, mutation_rate, mutation_probability, population, selection_percentage, cromossome_size):
+def mutate(networks, mutation_rate, mutation_probability, population, selection_percentage, cromossome_size, death_mutation):
     # Mutação
     num_old_members = int(population*selection_percentage)
-    rng = default_rng()
-    indices_populacao = np.arange(population)[num_old_members:]
-    mutated_networks = rng.choice(indices_populacao, size=int(len(indices_populacao)*mutation_probability), replace=False).tolist()
-    for network_idx in mutated_networks:
-        num_genes_mutados = int(cromossome_size*mutation_rate)
-        genes_sorteados = rng.choice(cromossome_size, size=num_genes_mutados, replace=False)
-        for gene in genes_sorteados:
+    if death_mutation:
+        rng = default_rng()
+        child1 = list(range(num_old_members,population,4))
+        child2 = list(range(num_old_members+1,population,4))
+        child1.extend(child2)
+        child1.sort()
+        child3 = list(range(num_old_members+2,population,4))
+        child4 = list(range(num_old_members+3,population,4))
+        child3.extend(child4)
+        child3.sort()
+        mutated_networks = rng.choice(child1, size=int(len(child1)*mutation_probability), replace=False).tolist()
+        for network_idx in mutated_networks:
+            num_genes_mutados = int(cromossome_size*mutation_rate)
             network = networks[network_idx]
-            network.actions[gene] = np.random.randint(10)
+            death_threshold = network.death_iteration-800
+            genes_in_death_threshold = np.arange(cromossome_size)[death_threshold:]
+            genes_sorteados = rng.choice(genes_in_death_threshold, size=num_genes_mutados, replace=False)
+            for gene in genes_sorteados:
+                network.actions[gene] = np.random.randint(10)
+
+        mutated_networks_two = rng.choice(child3, size=int(len(child3)*mutation_probability), replace=False).tolist()
+        for network_idx in mutated_networks_two:
+            num_genes_mutados = int(cromossome_size*mutation_rate)
+            genes_sorteados = rng.choice(cromossome_size, size=num_genes_mutados, replace=False)
+            for gene in genes_sorteados:
+                network = networks[network_idx]
+                network.actions[gene] = np.random.randint(10)
+    else:
+        rng = default_rng()
+        indices_populacao = np.arange(population)[num_old_members:]
+        mutated_networks = rng.choice(indices_populacao, size=int(len(indices_populacao)*mutation_probability), replace=False).tolist()
+        for network_idx in mutated_networks:
+            num_genes_mutados = int(cromossome_size*mutation_rate)
+            genes_sorteados = rng.choice(cromossome_size, size=num_genes_mutados, replace=False)
+            for gene in genes_sorteados:
+                network = networks[network_idx]
+                network.actions[gene] = np.random.randint(10)
 
     return networks
 
-def run(iteration, run_name, population, generations, self_crossover, mutation_rate, mutation_probability, selection_percentage, cromossome_size):
+def run(iteration, run_name, population, generations, self_crossover, mutation_rate, mutation_probability, selection_percentage, cromossome_size, death_mutation, reduced_action):
     print(f"Run {iteration}")
     print("-"*10)
 
@@ -248,7 +329,7 @@ def run(iteration, run_name, population, generations, self_crossover, mutation_r
     best_lucro_nets = []
     lucro_nets_media = []
     
-    networks = init_networks(population, cromossome_size)
+    networks = init_networks(population, cromossome_size, reduced_action)
     
     writer = SummaryWriter(log_dir='runs/'+run_name)
     t0=time.time()
@@ -258,7 +339,7 @@ def run(iteration, run_name, population, generations, self_crossover, mutation_r
         #print (f'Generation {gen+1}')
 
         #action
-        networks = fitness(networks)
+        networks = fitness(networks, reduced_action)
              
         network_lucro = []
         for network in networks:
@@ -268,8 +349,8 @@ def run(iteration, run_name, population, generations, self_crossover, mutation_r
 
         #Genetic
         networks = selection(networks, population, selection_percentage)
-        networks = crossover(networks, self_crossover, population, selection_percentage, cromossome_size)
-        networks = mutate(networks, mutation_rate, mutation_probability, population, selection_percentage, cromossome_size)
+        networks = crossover(networks, self_crossover, population, selection_percentage, cromossome_size, death_mutation, reduced_action)
+        networks = mutate(networks, mutation_rate, mutation_probability, population, selection_percentage, cromossome_size, death_mutation)
                 
         max_lucro = np.max(network_lucro)
         media = np.average(network_lucro)
@@ -325,18 +406,28 @@ def main():
     mutation_probability_values = [1] # 0.1, 0 muito baixos, não chegou a mutacionar mt #falta 0.5 e False p/ 0.05 e 0.1 m_r
     selection_percentage_values = [0.2]  #porcentagem dos melhores membros da população que irão pra próxima geração
     cromossome_size_values = [8000] #5000, 10000      #tamanho do cromossomo (numero de ações)
+    
+    population = 20
+    generations = 10
+    self_crossover = False
+    mutation_rate = 0.05
+    mutation_probability = 0.5
+    selection_percentage = 0.2
+    cromossome_size = 8000
     run_name="run"
     i=0
-    for population in population_values:
-        for generations in generations_values:
-            for self_crossover in self_crossover_values:
-                for mutation_rate in mutation_rate_values:
-                    for mutation_probability in mutation_probability_values:
-                        for selection_percentage in selection_percentage_values:
-                            for cromossome_size in cromossome_size_values:
-                                run_name = "run"+str(i)
-                                run(i, run_name, population, generations, self_crossover, mutation_rate, mutation_probability, selection_percentage, cromossome_size)
-                                i+=1
+    death_mutation = True
+    reduced_action_set = False
+    run(i, run_name, population, generations, self_crossover, mutation_rate, mutation_probability, selection_percentage, cromossome_size, death_mutation, reduced_action_set)
+    i+=1
+    death_mutation = False
+    reduced_action_set = True
+    run(i, run_name, population, generations, self_crossover, mutation_rate, mutation_probability, selection_percentage, cromossome_size, death_mutation, reduced_action_set)
+    i+=1
+    death_mutation = True
+    reduced_action_set = True
+    run(i, run_name, population, generations, self_crossover, mutation_rate, mutation_probability, selection_percentage, cromossome_size, death_mutation, reduced_action_set)
+    
 
 if __name__ == '__main__':
     main()
--- testes.py
+++ testes.py
@@ -3,6 +3,9 @@ from numpy.random import default_rng
 
 cromossome_size = 15
 
-print(type(np.random.randint(0, 10, size=cromossome_size).tolist()))
-
-print([10]*3+[5]*2)
+lista = np.arange(25).tolist()
+child1 = list(range(5,25,4))
+child2 = list(range(6,25,4))
+child1.extend(child2)
+child1.sort()
+print(child1)
